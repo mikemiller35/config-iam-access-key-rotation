d1a6931641645d8523a9a6615b623d0e8bd733bb
Mike Miller <mmiller@levelsbeyond.com>
2021-04-28 15:06:26 -0600

    fix(python): Fixed a bug when the event doesn't have a message

--- src/compliant_handler.py
+++ src/compliant_handler.py
@@ -24,11 +24,14 @@ def get_username(resourceId):
 
 
 def lambda_handler(event, context):
-    #
+    print(event)
     resourceId = event["detail"]["newEvaluationResult"]["evaluationResultIdentifier"][
         "evaluationResultQualifier"
     ]["resourceId"]
     complianceType = event["detail"]["newEvaluationResult"]["complianceType"]
-    configMessage = event["detail"]["newEvaluationResult"]["annotation"]
+    try:
+        configMessage = event["detail"]["newEvaluationResult"]["annotation"]
+    except KeyError:
+        configMessage = "No message."
     userName = get_username(resourceId)
     emailhandler.send_mesage(userName, configMessage, complianceType)
